683ab83863545d3b6c758941e12c0d08cfad3d8c
IlijaAngelov <ilija.angelov@yahoo.com>
2022-12-24 19:24:09 +0100

    Fix Form method for Delete

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -10,11 +10,11 @@
             <li>Title: {{ $article->title }}</li>
             <li>Text: {{ $article->text }}</li>
             <li>Image: {{ $article->image_path }}</li>
-            <li><a href="{{ route('article.edit', $article) }}" class="button">Edit</a></li>
-            <form action="POST" action={{ route('article.destroy', $article) }}>
+            <li><a href="{{ route('article.edit', $article) }}" class="px-6 py-3 font-semibold text-white no-underline bg-blue-500 rounded hover:bg-blue-600 hover:underline ">Edit</a></li>
+            <form method="POST" action={{ route('article.destroy', $article) }}>
                 @csrf
                 @method('DELETE')
-                <button type="submit" onclick="return confirm('Are you sure?')" class="h-10 px-6 font-semibold rounded-md bg-black text-white">Delete</button>
+                <button type="submit" onclick="return confirm('Are you sure?')" class="h-10 px-6 font-semibold rounded bg-black text-white hover:bg-slate-800 hover:underline">Delete</button>
             </form>
         </ul>
     </article>
