7846767b5480b8555769dd9ea179fb619ca69643
IlijaAngelov <ilija.angelov@yahoo.com>
2023-01-15 18:38:04 +0100

    Article Page Redesign

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,16 +8,15 @@
     </div>
 @endif
 
-<div class="">
+<div class="grid grid-cols-3 m-2 p-5 items-center h-56 text-center">
     @foreach ($articles as $article)
-    <article>
-        <ul class="h-56 grid grid-rows-3 gap-4 justify-items-start content-start p-5">
-            <li>Id: {{ $article->id }}</li>
-            <li><b>Title:</b> <a href="{{ route('article.show', $article) }}"> {{ $article->title }}</a></li>
-            <li>Text:{{ $article->text }}</li>
-            <li><img src="{{ asset('images/'.$article->image) }}" alt="{{ ($article->image) }}" title="article image" height="200px" width="200px"></li>
-        </ul>
-    </article>
+        <div class="flex justify-center w-auto m-2 border rounded-lg p-2">
+            <ul class="">
+                <li class="text-2xl font-black leading-5 hover:underline p-2"><a href="{{ route('article.show', $article) }}"><h1>{{ $article->title }}</h1></a></li>
+                <li class="text-lg p-2">{{ $article->text }}</li>
+                <li><img src="{{ asset('images/'.$article->image) }}" alt="{{ ($article->image) }}" title="article image" height="250px" width="250px"></li>
+            </ul>
+        </div>
     @endforeach
 </div>
 
