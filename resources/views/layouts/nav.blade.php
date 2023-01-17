67c2aea3166122625e827bc17918ecb2f505f693
IlijaAngelov <ilija.angelov@yahoo.com>
2023-01-17 16:29:43 +0100

    Separate route for index Change route path for index in nav bar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -69,7 +69,7 @@
     <div class="sm:hidden" id="mobile-menu">
         <div class="space-y-1 px-2 pt-2 pb-3">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="{{ route('article.index') }}" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+            <a href="{{ route('index') }}" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
                 aria-current="page">Home</a>
 
             @if(Auth::check())
--- routes/web.php
+++ routes/web.php
@@ -16,18 +16,7 @@ use App\Http\Controllers\ProfileController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('index');
-// });
-
 Route::resource('article', ArticleController::class)->middleware('auth');
-
-// Route::get('/', [ArticleController::class, 'index']);
-
-// Route::get('/article/create', [ArticleController::class, 'create'])->name('article.create');
-// Route::post('/article', [ArticleController::class, 'store'])->name('article.store');
-
-// Route::get('/article/show/{id}', [ArticleController::class, 'show'])->name('article.show');
 
 Route::get('/', [ArticleController::class, 'index'])->name('index');
 
